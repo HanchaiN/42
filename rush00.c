0650f0c3f3d66f6a63635d3848a34baca78c5618
Hanchai Nonprasart <hnonpras@z2t9c2.42bangkok.com>
2023-06-10 14:27:01 +0700

    implement `rush`

--- rush00.c
+++ rush00.c
@@ -6,7 +6,7 @@
 /*   By: hnonpras <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 13:26:13 by hnonpras          #+#    #+#             */
-/*   Updated: 2023/06/10 13:39:49 by tplaengm         ###   ########.fr       */
+/*   Updated: 2023/06/10 14:25:44 by hnonpras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,28 @@ void	ft_putchar(char c);
 
 void	rush(int x, int y)
 {
-	ft_putchar('a');
+	int	i;
+	int	j;
+
+	j = 0;
+	while (j++ < y)
+	{
+		i = 0;
+		while (i++ < x)
+		{
+			if ((i == 1 && j == 1)
+				|| (i == 1 && j == y)
+				|| (i == x && j == 1)
+				|| (i == x && j == y))
+				ft_putchar('o');
+			else if (i == 1 || i == x)
+				ft_putchar('|');
+			else if (j == 1 || j == y)
+				ft_putchar('-');
+			else
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+	}
 	return ;
 }
