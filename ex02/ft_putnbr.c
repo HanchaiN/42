4fbb46ebdf60041fccd0b963ed0f994628aee53b
Hanchai Nonprasart <hnonpras@z2t9c2.42bangkok.com>
2023-06-11 15:35:21 +0700

    Initial commit

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnonpras <hnonpras@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 09:15:33 by hnonpras          #+#    #+#             */
+/*   Updated: 2023/06/11 14:14:22 by hnonpras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(STDOUT_FILENO, &c, 1);
+	return ;
+}
+
+void	_putnbr(int nb)
+{
+	if (nb == 0)
+		return ;
+	_putnbr(nb / 10);
+	if (nb > 0)
+		ft_putchar('0' + nb % 10);
+	else
+		ft_putchar('0' - nb % 10);
+	return ;
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == 0)
+	{
+		ft_putchar('0');
+	}
+	else if (nb > 0)
+	{
+		_putnbr(nb);
+	}
+	else
+	{
+		ft_putchar('-');
+		_putnbr(-nb);
+	}
+	return ;
+}
---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnonpras <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/11 14:24:55 by hnonpras          #+#    #+#             */
+/*   Updated: 2023/06/11 15:34:51 by hnonpras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	_strlen(char *str)
+{
+	int		count;
+	char	*ptr;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		ptr = str + 1;
+		while (*ptr != '\0')
+		{
+			if (*ptr == *str)
+				return (-1);
+			ptr++;
+		}
+		if (*str == '+' || *str == '-' || *str <= ' ' || *str > '~')
+			return (-1);
+		str++;
+		count++;
+	}
+	if (count < 2)
+		return (-1);
+	return (count);
+}
+
+void	ft_putchar(char c)
+{
+	write(STDOUT_FILENO, &c, 1);
+	return ;
+}
+
+void	_putnbr_base(int nb, int radix, char *base)
+{
+	if (nb == 0)
+		return ;
+	_putnbr_base(nb / radix, radix, base);
+	if (nb > 0)
+		ft_putchar(base[nb % radix]);
+	else
+		ft_putchar(base[-nb % radix]);
+	return ;
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	radix;
+
+	radix = _strlen(base);
+	if (radix < 0)
+		return ;
+	if (nbr == 0)
+	{
+		ft_putchar(base[0]);
+	}
+	else if (nbr > 0)
+	{
+		_putnbr_base(nbr, radix, base);
+	}
+	else
+	{
+		ft_putchar('-');
+		_putnbr_base(-nbr, radix, base);
+	}
+	return ;
+}
