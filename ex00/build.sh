24efcd721380d1b80c320b24c6130cf43bc101f8
Hanchai Nonprasart <hnonpras@z2t1c1.42bangkok.com>
2023-06-16 12:21:25 +0700

    fixup! feat: add build.sh for build and run

--- ex00/build.sh
+++ ex00/build.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-if !(cc -Wall -Wextra -Werror -o rush-01 *.c); then
-	exit $?
-fi
+trap cleanup EXIT
 
-printf '%s' "Arguments: "
-while IFS='\n' read -r arg; do
-	./rush-01 "$arg"
-	if [ -z "$arg" ]; then
-		break
+function cleanup {
+	printf '\n'
+	printf 'Cleaning up...\n'
+	[ -e rush-01 ] && rm rush-01
+	printf 'Cleaning done.'
+}
+
+function main {
+	printf 'Compiling the program...\n'
+	if !(cc -Wall -Wextra -Werror -o rush-01 *.c); then
+		exit $?
 	fi
-	printf '%s' "Arguments: "
-done
+	printf 'Compilation done.\n'
+
+	printf 'Test the program by typing the arguments, press Ctrl+C to exit.\n'
+	printf 'Arguments: '
+	while IFS='\n' read -r args; do
+		echo ${args[@]} | xargs ./rush-01
+		printf 'Arguments: '
+	done
+}
+
+main
+
