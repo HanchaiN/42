da4c27354ab785ca2c32dbc4f7a3e1ecd304338f
Kongrith Komasatid <kkomasat@z2t15c1.42bangkok.com>
2023-06-26 15:32:00 +0700

    refractor

--- ex00/srcs/core/ft_parse_number.c
+++ ex00/srcs/core/ft_parse_number.c
@@ -6,39 +6,30 @@
 /*   By: hnonpras <hnonpras@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 19:37:11 by hnonpras          #+#    #+#             */
-/*   Updated: 2023/06/25 19:37:17 by hnonpras         ###   ########.fr       */
+/*   Updated: 2023/06/26 15:14:04 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_dict.h"
 #include <stdlib.h>
 
-unsigned int	ft_parse_number(char string[], int *error_flag)
+unsigned int	handle_number(char string[], int *error_flag,
+				unsigned int *input_number, int *i)
 {
-	unsigned int input_number;
-	int i;
-
-	if (string == NULL)
+	while (string[*i] != '\0')
 	{
-		*error_flag = 1;
-		return (0);
-	}
-	input_number = 0;
-	i = 0;
-	while (string[i] != '\0')
-	{
-		if ('0' <= string[i] && string[i] <= '9')
+		if ('0' <= string[*i] && string[*i] <= '9')
 		{
-			if (input_number >= 429496730 || (input_number >= 429496729
-					&& string[i] > '5'))
+			if (*input_number >= 429496730 || \
+						(*input_number >= 429496729 && string[*i] > '5'))
 			{
 				*error_flag = 1;
 				return (0);
 			}
 			else
 			{
-				input_number = input_number * 10 + (string[i] - 48);
-				++i;
+				*input_number = 10 * *input_number + (string[*i] - 48);
+				++*i;
 			}
 		}
 		else
@@ -47,5 +38,21 @@ unsigned int	ft_parse_number(char string[], int *error_flag)
 			return (0);
 		}
 	}
+	return (*input_number);
+}
+
+unsigned int	ft_parse_number(char string[], int *error_flag)
+{
+	unsigned int	input_number;
+	int				i;
+
+	input_number = 0;
+	i = 0;
+	if (string == NULL)
+	{
+		*error_flag = 1;
+		return (0);
+	}
+	input_number = handle_number(string, error_flag, &input_number, &i);
 	return (input_number);
 }
