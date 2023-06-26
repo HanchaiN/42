8a5c016996137ba74ab6d8429233a1904bf55cb1
Stanislav Liashkov <sliashko@z2t7c1.42bangkok.com>
2023-06-26 11:40:40 +0700

    1. Created a header file for convenience 2. Created file read_file.c which is repsonsible    for reading file to array of chars 3. Created a file utils.c which going to contain some useful funcs

---
+++ read_file.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_file.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliashko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 11:10:25 by sliashko          #+#    #+#             */
+/*   Updated: 2023/06/26 11:10:27 by sliashko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq_funcs.h"
+
+//This function gets file descriptor
+int	get_fd(char *path)
+{
+	int	fd;
+
+	fd = open(path, O_RDONLY);
+	return (fd);
+}
+
+//gets the size of file by given file descriptor
+ssize_t	get_size(int fd)
+{
+	char	buffer[1024];
+	ssize_t	chunk_sz;
+	ssize_t	total_sz;
+
+	chunk_sz = 1;
+	total_sz = chunk_sz;
+	while (chunk_sz > 0)
+	{
+		chunk_sz = read(fd, buffer, sizeof(buffer));
+		if (chunk_sz == -1)
+			return (-1);
+		total_sz = total_sz + chunk_sz;
+	}
+	close(fd);
+	return (total_sz - 1);
+}
+
+//Return the read file written in array of chars
+// MUST BE FREED AFTER USAGE
+char	*get_content(char *path)
+{
+	char	*content;
+	int		fd;
+	ssize_t	size;
+
+	fd = get_fd(path);
+	if (fd == -1)
+	{
+		ft_putstr("Failed to open the file\n");
+		return (NULL);
+	}
+	size = get_size(fd);
+	fd = get_fd(path);
+	content = (char *) malloc(size);
+	if (read(fd, content, size) != size)
+	{
+		ft_putstr("Failed to read content\n");
+		return (NULL);
+	}
+	close(fd);
+	return (content);
+}
+
+//UNCOMMENT TO TEST READING FILE TO ARRAY OF CHARS
+/*
+int	main()
+{
+	char	*content;
+
+	content = get_content("test.txt");
+	printf("%s", content);
+}
+*/
