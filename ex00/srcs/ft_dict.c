95f7e1b004e5bfe2e45417ad80e08de9e4ed80eb
Kongrith Komasatid <kkomasat@z2t15c1.42bangkok.com>
2023-06-26 21:46:27 +0700

    solve error

--- ex00/srcs/ft_dict.c
+++ ex00/srcs/ft_dict.c
@@ -6,7 +6,7 @@
 /*   By: hnonpras <hnonpras@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 15:54:32 by hnonpras          #+#    #+#             */
-/*   Updated: 2023/06/26 09:34:25 by hnonpras         ###   ########.fr       */
+/*   Updated: 2023/06/26 21:06:24 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,51 @@
 #include "ft_dict.h"
 #include <stdlib.h>
 #include <unistd.h>
+/*
+void	manage_error(int error_flag, t_buffer buffef, unsigned int nbr, char dict_path)
+{
+	if (error_flag)
+	{
+		write(STDERR_FILENO, "Error\n", 6);
+		return (1);
+	}
+	buffer = ft_buffer_new(0);
+	if (ft_put_number(nbr, dict_path, buffer))
+	{
+		error_flag = 1;
+		write(STDERR_FILENO, "Dict Error\n", 11);
+	}
+	else
+	{
+		ft_buffer_putchar('\n', buffer);
+		ft_buffer_print(buffer);
+	}
+}*/
+/*
+void	manage_agruments(int argc, char **argv, char *str, int *error_flag)
+{
+	int				read_count;
+	char			*dict_path;
 
+	dict_path = (char *) g_default_dict;
+	if (argc == 1)
+	{
+		*str = (char *) malloc(1024 * sizeof(char));
+		read_count = read(STDIN_FILENO, *str, 1024);
+		if (*str[read_count - 1] == '\n')
+			*str[read_count - 1] = '\0';
+	}
+	else if (argc == 2)
+		*str = ft_strdup(argv[1]);
+	else if (argc == 3)
+	{
+		dict_path = argv[1];
+		*str = ft_strdup(argv[2]);
+	}
+	else
+		*error_flag = 1;
+}
+*/
 int	main(int argc, char **argv)
 {
 	t_buffer		*buffer;
@@ -27,6 +71,8 @@ int	main(int argc, char **argv)
 
 	error_flag = 0;
 	str = NULL;
+	//manage_agruments(argc, argv, str, &error_flag);
+	
 	dict_path = (char *) g_default_dict;
 	if (argc == 1)
 	{
@@ -44,8 +90,12 @@ int	main(int argc, char **argv)
 	}
 	else
 		error_flag = 1;
+	
 	nbr = ft_parse_number(str, &error_flag);
 	free(str);
+
+	//manage_error(error_flag, buffer);
+	
 	if (error_flag)
 	{
 		write(STDERR_FILENO, "Error\n", 6);
@@ -62,6 +112,7 @@ int	main(int argc, char **argv)
 		ft_buffer_putchar('\n', buffer);
 		ft_buffer_print(buffer);
 	}
+	
 	ft_buffer_free(buffer);
 	return (error_flag);
 }
--- ex00/srcs/locales/en/ft_dict_format.c
+++ ex00/srcs/locales/en/ft_dict_format.c
@@ -6,20 +6,76 @@
 /*   By: hnonpras <hnonpras@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 09:18:34 by hnonpras          #+#    #+#             */
-/*   Updated: 2023/06/25 16:44:15 by hnonpras         ###   ########.fr       */
+/*   Updated: 2023/06/26 21:08:39 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "ft_buffer.h"
 #include "ft_dict.h"
-#define MYRIAD (10000)
+#define THOUSAND (1000)
 
-const char	*g_default_dict = "./numbers.en.dict";
+const char *g_default_dict = "./numbers.en.dict";
+
+static int	_put_number_group(unsigned int nbr, const char *path, t_buffer *buffer)
+{
+	if (nbr / 100)
+	{
+		if (ft_put_number(nbr / 100, path, buffer))
+			return (1);
+		ft_buffer_putstr(" ", buffer);
+		if (ft_put_number(100, path, buffer))
+			return (1);
+		if (nbr % 100)
+			ft_buffer_putstr(" and ", buffer);
+	}
+	if ((nbr % 100) / 10 == 1)
+		return (ft_put_value(nbr % 100, path, buffer));
+	if (ft_put_value(nbr % 100 / 10, path, buffer))
+		return (1);
+	if ((nbr % 100) / 10 && nbr % 10)
+		ft_buffer_putstr("-", buffer);
+	if (ft_put_value(nbr % 10, path, buffer))
+		return (1);
+	return (0);
+}
+
+static unsigned int	_get_group_digit_value(unsigned int nbr)
+{
+	unsigned int	digit_value;
+
+	digit_value = 1;
+	nbr /= THOUSAND;
+	while (nbr > 0)
+	{
+		nbr /= THOUSAND;
+		digit_value *= THOUSAND;
+	}
+	return (digit_value);
+}
 
 int	ft_put_number(unsigned int nbr, const char *path, t_buffer *buffer)
 {
-	(void)nbr;
-	(void)path;
-	(void)buffer;
-	return (1);
+	unsigned int	digit_value;
+	unsigned int	group_value;
+
+	if (nbr == 0)
+		return (ft_put_value(nbr, path, buffer));
+	digit_value = _get_group_digit_value(nbr);
+	while (1)
+	{
+		group_value = (nbr / digit_value) % THOUSAND;
+		if (_put_number_group(group_value, path, buffer))
+			return (1);
+		if (digit_value <= 1)
+			break ;
+		if (group_value)
+		{
+			ft_buffer_putstr(" ", buffer);
+			if (ft_put_value(digit_value, path, buffer))
+				return (1);
+		}
+		if (nbr % digit_value)
+			ft_buffer_putstr(" ", buffer);
+		digit_value /= THOUSAND;
+	}
+	return (0);
 }
